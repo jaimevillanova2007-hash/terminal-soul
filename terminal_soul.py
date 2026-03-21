3d865ee47bba5b7f7dd51ecb5fdb7ef56521582c
Andres Barrios <anmabadi@outlook.com>
2026-03-21 11:58:43 -0500

    juego terminado

--- terminal_soul.py
+++ terminal_soul.py
@@ -3,25 +3,17 @@ import random
 def generate_damage(min:int, max:int):
     return random.randint(min,max)
 
+
 def show_state(name_h:str, hp_h:int, name_e:str, hp_e:int):
+    print("\n    STATE    \n")
+    print(name_h, "| Hp:", hp_h)
+    print(name_e, "| Hp:", hp_e)
+    print()
 
-    visual_bar_h = 10
 
-    remaining_life_h = hp_h // visual_bar_h
-    remaing_demage_h = visual_bar_h - (hp_h // visual_bar_h)
-
-    print("Name:", name_h, "[" + "#" * remaining_life_h + "-" * remaing_demage_h + "]" )
-
-    visual_bar_e = 12
-
-    remaining_life_e = hp_e // visual_bar_e
-    remaing_demage_e = visual_bar_e - (hp_e // visual_bar_e)
-
-    print("Name:", name_e, "[" + "#" * remaining_life_e + "-" * remaing_demage_e + "]" )
-
-def player_turn(hp_hero):
+def player_turn(hp_h:int, hp_e:int,potions:int):
     
-    potion = 3
+   
     op = input("Choose an option to take:\n" \
     "1. Attack\n" \
     "2. Heal\n" \
@@ -29,26 +21,18 @@ def player_turn(hp_hero):
     "-> ").lower()
 
     if op == "attack" or op == "1":
-
-        print("You have hit by:", generate_damage(10, 25), "point of damages")
+        damage_hero = generate_damage(10, 25)
+        hp_e -= damage_hero
+        print("You have hit by:",damage_hero , "points of damage")
 
     elif op == "heal" or op == "2":
         
-        if potion == 3:
-            potion -= 1
-            hp_hero += 20
-            print("You have 2 potions left and have been healed by +20 Hp")
-        elif potion == 2:
-            potion -= 1
-            hp_hero += 20
-            print("You have 1 potions left and have been healed by +20 Hp")
-        elif potion == 1:
-            potion -= 1
-            hp_hero += 20
-            print("You just used all potions and have been healed by +20 Hp")
+        if potions > 0:
+            hp_h += 20
+            potions -= 1
+            print("You have been healed  by +20HP points of health")
         else:
-            print("What are you doing? No potions!")
-
+            print("No potions left!")
 
     elif op == "special ability" or op == "3":
 
@@ -56,40 +40,59 @@ def player_turn(hp_hero):
         if probability == 0:
             print("You have failed")
         else:
-            print("You have attacked by ", generate_damage(30,50), "point of damages")
-  
-
-        print("You have been healed  by", hp_hero,"points of health")
-    
+            damage_hero = generate_damage(30,50)
+            hp_e -= damage_hero
+            print("You have attacked by ", damage_hero, "points of damage")
+       
     else:
-        print("\nChoose the key word or a number")
-    
-    
-    
+        print("\nChoose the key word or a number") 
 
-def enemy_turn (hp_h):
+    return hp_h, hp_e, potions   
+
+
+def enemy_turn (hp_h:int):
     enemy_damage = generate_damage(15, 20)
     hp_h -= enemy_damage
     print("Enemy has hit you by -", enemy_damage,"points of damage")
-    
+
+    return hp_h
+
 
 def verify_winner(hp_h:int,hp_e:int):
-    if hp_h == 0 or hp_e == 0:
+
+    if hp_h <= 0:
+        print("\nYou lost :(")
+        return True
+    
+    elif hp_e <= 0:
+        print("\nYou won! :)")
         return True
 
-name_hero = input("Choose your hero's name: ") # inicialized with input
-name_enemy =  "Ares" # inicialized so it uses later on input
+    else:
+        return False
+
+
+print("Street Fighter - Badass Version")
+name_enemy =  "Ares" 
+print("\nYour Enemy's name:", name_enemy)
+name_hero = input("\nChoose your hero's name: ")
+
 hp_hero = 100 # inicialized hero's hp 100 by default
 hp_enemy = 120 # inicialized enemy's hp 120 by default.
+potions = 3
+
 game_over = False
-while game_over != True:
+
+while game_over == False:
+    hp_hero,hp_enemy,potions = player_turn(hp_hero, hp_enemy, potions) # storing what function is giving
+    game_over = verify_winner(hp_hero,hp_enemy)
+    if game_over == False:
+        hp_hero = enemy_turn(hp_hero)
+        game_over = verify_winner(hp_hero,hp_enemy)
     
-    player_turn(hp_hero)
+    show_state(name_hero,hp_hero,name_enemy,hp_enemy)
 
-    enemy_turn(hp_hero)
 
-    show_state(name_hero, hp_hero, name_enemy, hp_enemy)
 
-    verify_winner(hp_hero, hp_enemy)
     
 
